f1e80039215c79e5b9ecff16e846c8c6c6e6717b
selastmomo <lizeyu168@icloud.com>
2021-11-26 21:01:10 +0800

    Update router.go

--- api/router.go
+++ api/router.go
@@ -1,8 +1,8 @@
 package api
 
 import (
-	binhtml "github.com/Sansui233/proxypool/internal/bindata/html"
-	"github.com/Sansui233/proxypool/log"
+	binhtml "github.com/selastmomo/proxypool/internal/bindata/html"
+	"github.com/selastmomo/proxypool/log"
 	"html/template"
 	"net/http"
 	"os"
@@ -10,9 +10,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/Sansui233/proxypool/config"
-	appcache "github.com/Sansui233/proxypool/internal/cache"
-	"github.com/Sansui233/proxypool/pkg/provider"
+	"github.com/selastmomo/proxypool/config"
+	appcache "github.com/selastmomo/proxypool/internal/cache"
+	"github.com/selastmomo/proxypool/pkg/provider"
 	"github.com/gin-contrib/cache"
 	"github.com/gin-contrib/cache/persistence"
 	"github.com/gin-gonic/gin"
