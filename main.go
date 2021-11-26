21e0e6306f0db522e319b0e4063beef7f70a21ef
selastmomo <lizeyu168@icloud.com>
2021-11-26 20:59:00 +0800

    Rebuild

--- main.go
+++ main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"flag"
-	"github.com/Sansui233/proxypool/pkg/geoIp"
+	"github.com/selastmomo/proxypool/pkg/geoIp"
 	_ "net/http/pprof"
 	"os"
 
-	"github.com/Sansui233/proxypool/api"
-	"github.com/Sansui233/proxypool/internal/app"
-	"github.com/Sansui233/proxypool/internal/cron"
-	"github.com/Sansui233/proxypool/internal/database"
-	"github.com/Sansui233/proxypool/log"
+	"github.com/selastmomo/proxypool/api"
+	"github.com/selastmomo/proxypool/internal/app"
+	"github.com/selastmomo/proxypool/internal/cron"
+	"github.com/selastmomo/proxypool/internal/database"
+	"github.com/selastmomo/proxypool/log"
 )
 
 var configFilePath = ""
